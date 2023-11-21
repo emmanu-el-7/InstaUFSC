b4ba3204aa39010262a520b290453d1b9659e51c
Emmanuel Rodrigues da Rocha Costa <rodriguesemanuel021@gmail.com>
2023-11-21 20:41:24 -0300

    node-modules

---
+++ instaufsc/frontend/src/components/Navbar.jsx
@@ -0,0 +1,96 @@
+import "./Navbar.css";
+
+import { NavLink } from "react-router-dom";
+import {
+  BsSearch,
+  BsHouseDoorFill,
+  BsFillPersonFill,
+  BsFillCameraFill,
+} from "react-icons/bs"; //importação dos ícones da biblioteca Bootstrap
+
+import { useState } from "react";
+import { useAuth } from "../hooks/useAuth";
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { logout, reset } from "../slices/authSlice";
+
+const Navbar = () => {
+  const { auth } = useAuth(); //pegamos a informação sobre autenticação do nosso hook
+  const { user } = useSelector((state) => state.auth); //pegamos o usuário do initialState do slice
+
+  const [query, setQuery] = useState("");
+
+  const navigate = useNavigate();
+
+  const dispatch = useDispatch(); //o dispatch é quem nos possibilita chamarmos as funções Redux
+
+  const handleLogout = () => {
+    //ação única de logout do sistema
+    dispatch(logout());
+    dispatch(reset());
+
+    navigate("login"); //depois da desconexão, redirecionamos o usuário para a tela de login
+  };
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+
+    if (query) {
+      return navigate(`search?q=${query}`);
+    }
+  };
+
+  return (
+    <nav id="nav">
+      <NavLink to={"/"}>InstaUFSC</NavLink>
+      <form id="search-form" onSubmit={handleSearch}>
+        <BsSearch />
+        <input
+          type="text"
+          placeholder="Pesquisar"
+          onChange={(e) => setQuery(e.target.value)}
+        />
+      </form>
+      <ul id="nav-links">
+        {auth ? (
+          <>
+            <li>
+              <NavLink to={"/"}>
+                <BsHouseDoorFill />
+              </NavLink>
+            </li>
+            {/* link para a página de usuário */}
+            {user && (
+              <li>
+                <NavLink to={`/users/${user._id}`}>
+                  <BsFillCameraFill />
+                </NavLink>
+              </li>
+            )}
+            {/* link para a página de perfil de usuário */}
+            <li>
+              <NavLink to={"/profile"}>
+                <BsFillPersonFill />
+              </NavLink>
+            </li>
+            {/* link para o logout */}
+            <li>
+              <span onClick={handleLogout}>Sair</span>
+            </li>
+          </>
+        ) : (
+          <>
+            <li>
+              <NavLink to={"/login"}>Entrar</NavLink>
+            </li>
+            <li>
+              <NavLink to={"/register"}>Cadastrar</NavLink>
+            </li>
+          </>
+        )}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
